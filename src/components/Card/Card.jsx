2d1ae03430b13861e3ce607eef25ad52d7616fc3
Ashvin Bambhaniya <ashvinbambhaniya2003@gmail.com>
2023-02-23 18:44:04 +0530

    Portfoliyo create and also deploy it

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Card.css";
+import { useContext } from "react";
+import { themeContext } from "../../Context";
+
+const Card = ({ emoji, heading, detail, color }) => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  return (
+    <div style={{ background: darkMode ? "rgb(42 52 60)" : "" }} className="card">
+      <img src={emoji} alt="" />
+      <span>{heading}</span>
+      <span>{detail}</span>
+      <button className="c-button">LEARN MORE</button>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,88 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>services</span>
+        <span>
+          My Js and React knowledge is good and I create Website using React Library
+          <br />
+          Also quite good in HTML, CSS and BootStrap.
+        </span>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Frontend"}
+            detail={"Html, Css, Bootstrap Framework, JavaScript, React Library"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Backend "}
+            // detail={"Html, Css, JavaScript, React, Nodejs, Express"}
+            detail={"JavaScript, Nodejs Framework, Express, Mongodb"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Design"}
+            detail={"Figma, Sketch, Photoshop, Adobe Illustrator, Adobe xd"}
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
