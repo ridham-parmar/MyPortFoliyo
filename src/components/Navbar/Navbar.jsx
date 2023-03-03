208b6c391e0184f9f223b7033d85091cbd66907a
Ashvin Bambhaniya <ashvinbambhaniya2003@gmail.com>
2023-03-03 23:13:14 +0530

    Navbar sicky and mobile view better

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,50 +3,52 @@ import Toggle from "../Toggle/Toggle";
 import "./Navbar.css";
 import { Link } from "react-scroll";
 import { motion } from "framer-motion";
-import { headerVariants } from "../../utils/motion";
+import { fadeIn, headerVariants, staggerContainer } from "../../utils/motion";
 
 
 const Navbar = () => {
 
   return (
     <motion.div className="n-wrapper" id="Navbar"
-      variants={headerVariants}
+      variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       // className={`bg-primary paddings ${css.wrapper}`}
       viewport={{ once: true, amount: 0.25 }}
     >
       {/* left */}
-      <div className="n-left">
+      <motion.div className="n-left"
+        variants={fadeIn("down", "tween", 1, 1)}>
         <div className="n-name">Ashvin</div>
         <Toggle />
-      </div>
+      </motion.div>
       {/* right */}
-      <div className="n-right">
+      <motion.div className="n-right"
+        variants={fadeIn("down", "tween", 1, 1)}>
         <div className="n-list">
           <ul style={{ listStyleType: "none" }}>
             <li>
-              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+              <Link className="nav-font-bold" activeClass="active" to="Navbar" spy={true} smooth={true}>
                 Home
               </Link>
             </li>
             <li>
-              <Link to="skiils" spy={true} smooth={true}>
+              <Link className="nav-font-bold" to="skiils" spy={true} smooth={true}>
                 Skills
               </Link>
             </li>
             <li>
-              <Link to="education" spy={true} smooth={true}>
+              <Link className="nav-font-bold" to="education" spy={true} smooth={true}>
                 Education
               </Link>
             </li>
             <li>
-              <Link to="languages" spy={true} smooth={true}>
+              <Link className="nav-font-bold" to="languages" spy={true} smooth={true}>
                 Languages
               </Link>
             </li>
             <li>
-              <Link to="portfolio" spy={true} smooth={true}>
+              <Link className="nav-font-bold" to="portfolio" spy={true} smooth={true}>
                 Portfolio
               </Link>
             </li>
@@ -60,7 +62,7 @@ const Navbar = () => {
         <Link to="contact" spy={true} smooth={true}>
           <button className="button n-button">Contact</button>
         </Link>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
